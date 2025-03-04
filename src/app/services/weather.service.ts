2025a97cb92ee8e01f034020e94693900754f861
Dam Pun <pun7dam@gmail.com>
2025-03-04 21:12:43 +1100

    added environment file

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,24 +1,26 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+const apiKey = environment.apiKey;
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
-  private apiKey = '9a420aacc4a453b8b846e7a58fbaefca';
   private apiUrl = 'https://api.openweathermap.org/data/2.5';
   private http: HttpClient = inject(HttpClient);
 
   getCurrentWeather(city: string, unit = 'metric'): Observable<any> {
     return this.http.get(
-      `${this.apiUrl}/weather?q=${city}&units=${unit}&appid=${this.apiKey}`
+      `${this.apiUrl}/weather?q=${city}&units=${unit}&appid=${apiKey}`
     );
   }
 
   getForecast(city: string, unit = 'metric'): Observable<any> {
     return this.http.get(
-      `${this.apiUrl}/forecast?q=${city}&units=${unit}&appid=${this.apiKey}`
+      `${this.apiUrl}/forecast?q=${city}&units=${unit}&appid=${apiKey}`
     );
   }
 }
