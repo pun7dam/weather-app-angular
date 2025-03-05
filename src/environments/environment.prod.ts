d5aa0e907898688cce0142bb652efe254b4c6f29
Dam Pun <pun7dam@gmail.com>
2025-03-05 20:59:39 +1100

    fixing deployment to vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiKey: 'your_api_key_here',
+  apiKey: '',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiKey: 'your_api_key_here',
+  apiKey: '',
 };
